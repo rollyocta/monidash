2694f337ba35db9c7277491c5e925a68d781f660
Rolly <rollyocta0205@gmail.com>
2026-02-07 14:30:30 +0800

    responsive welcome page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -6,20 +6,22 @@
 
 <main class="welcome-container">
     <div class="logo">
-        <img src="{{ asset('images/moni/moni-logo.png') }}" alt="Moni Logo">
-        <h1>MoniDash</h1>
+        <img src="{{ asset('images/moni/monidash-logo.png') }}" alt="Moni Logo">
     </div>
     <section class="welcome-section">
         <div class="get-started">
+            <div class="moni-wave">
+                <img src="{{ asset('images/moni/moni-wave.png') }}" alt="Moni Image">
+            </div>
             <div class="intro-welcome">
                 <h1>Track. Save. Grow</h1>
                 <h4>let's make every cent count</h4>
-                <p>Smart tracking for a smarter you. Your budget's new best friend is ready.</p>
+                <p>Smart tracking for a smarter you. Your budget's <br> new best friend is ready. </p>
                 <div>
                     <a href="#" class="get-started-btn">Get Started</a>
                 </div>
             </div>
-            <div class="intro-moni">
+            <div class="moni-float">
                 <img src="{{ asset('images/moni/moni-float.png') }}" alt="Moni Image">
             </div>
         </div>
